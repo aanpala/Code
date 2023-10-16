fb690db627d069d03b9caa5302d4531aae165225
Arun Anpalagan <arunco98@gmail.com>
2023-10-16 12:31:17 -0400

    Summation Code

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Define a variable to store the sum
+total_sum = 0
+
+# Use a for loop to iterate from 1 to 10 and add each number to the sum
+for number in range(1, 11):
+    total_sum += number
+
+# Print the result
+print("The Sum of numbers from 1 to 10 is:", total_sum)

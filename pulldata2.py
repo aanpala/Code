077ee070b7bb1c505fa6f6ae26d9707bf6c37bfa
Arun Anpalagan <44444757+aanpala@users.noreply.github.com>
2023-10-28 17:51:15 -0400

    Add files via upload
    updated insert

--- pulldata2.py
+++ pulldata2.py
@@ -2,6 +2,7 @@ import yfinance as yf
 from SP500Wiki import *
 from methodsNEW import *
 
+
 conn = establish_database_connection()
 cur = conn.cursor()
 
@@ -21,7 +22,6 @@ cashflow_Y = pd.DataFrame()
 
 
 # IS quarterly
-# DONE
 for ticker_symbol in tickers:
     ticker = yf.Ticker(ticker_symbol)
     df = ticker.quarterly_income_stmt.T
@@ -33,6 +33,7 @@ for ticker_symbol in tickers:
     if not df.empty:
         cur = conn.cursor()
         for _, row in df.iterrows():  # Iterate through all rows of data
+            print(ticker)
             update_or_insert_yf_income_statement_data(cur, ticker_symbol, row)
         conn.commit()  # Commit changes to the database
         cur.close()
@@ -42,26 +43,9 @@ for ticker_symbol in tickers:
     income_statement_Q = pd.concat([income_statement_Q, df])
 
 # Display the combined data for all stocks
-print(income_statement_Q)
+#print(income_statement_Q)
    
-# IS annual
-# for ticker_symbol in tickers:
-#     ticker = yf.Ticker(ticker_symbol)
-#     df = ticker.income_stmt.T
-#     df['Ticker'] = ticker_symbol
-#     df['Date'] = df.index.strftime('%Y-%m-%d')
-#     if not df.empty:
-#         cur = conn.cursor()
-#         for _, row in df.iterrows():  # Iterate through all rows of data
-#             pass
-#             #update_or_insert_yf_income_statement_data(cur, ticker_symbol, row)
-#         conn.commit()  # Commit changes to the database
-#         cur.close()
-#     #print(df)
-
-#    income_statement_Y = pd.concat([income_statement_Y, df])
-
-#print(income_statement_Y)
+#IS annual
 
 # BS quarterly
 for ticker_symbol in tickers:
@@ -79,20 +63,6 @@ for ticker_symbol in tickers:
     # Concatenate the data to your income_statement_Q DataFrame
     balance_sheet_Q = pd.concat([balance_sheet_Q, df])
 
-# Display the combined data for all stocks
-#print(balance_sheet_Q)
-
-# # BS annual
-# for ticker_symbol in tickers:
-#     ticker = yf.Ticker(ticker_symbol)
-#     df = ticker.balance_sheet.T
-#     df['Ticker'] = ticker_symbol
-#     df['Date_Ticker'] = df['Ticker'] + "." +  df.index.strftime('%Y-%m-%d')     #
-#     df = df.reset_index(drop=True)
-#     balance_sheet_Y = pd.concat([balance_sheet_Y, df])
-#     balance_sheet_Y = balance_sheet_Y.reset_index(drop=True)
-
-# #print(balance_sheet_Y)
 
 
 # CF quarterly
@@ -114,43 +84,3 @@ for ticker_symbol in tickers:
 #print(cashflow_Q)
 
 # # CF annual
-# for ticker_symbol in tickers:
-#     ticker = yf.Ticker(ticker_symbol)
-#     df = ticker.cashflow.T
-#     df['Ticker'] = ticker_symbol
-#     df['Date_Ticker'] = df['Ticker'] + "." +  df.index.strftime('%Y-%m-%d') 
-#     #df = df.reset_index(drop=True)
-#     cashflow_Y = pd.concat([cashflow_Y, df])
-#     cashflow_Y = cashflow_Y.reset_index(drop=True)
-
-# #print(cashflow_Y)
-
-
-
-
-
-
-# # show meta information about the history (requires history() to be called first)
-# #print(msft.history_metadata)
-
-
-# #print(msft.actions)
-# #print(msft.dividends)
-# #print(msft.splits)
-# #print(msft.capital_gains)  # only for mutual funds & etfs
-
-# # show share count
-# #print(msft.get_shares_full(start="2022-01-01", end=None))
-
-# # show financials:
-# # - income statement
-# #print(msft.income_stmt)
-# #print(msft.quarterly_income_stmt)
-# # - balance sheet
-# #print(msft.balance_sheet)
-# #print(msft.quarterly_balance_sheet)
-# # - cash flow statement
-# #print(msft.cashflow)
-# #print(msft.quarterly_cashflow)
-
-# #print(msft.earnings_dates)
